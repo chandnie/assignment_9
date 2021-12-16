224903084f9d182c594176a0b5e5f6ea9bc8da41
chandnie <95160283+chandnie@users.noreply.github.com>
2021-12-16 21:24:25 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Button from "./Button";
 const Header = ({ title, onAdd, showAdd }) => {
   const location = useLocation();
 
+  
   return (
     <header className="header">
       <h1>{title}</h1>
@@ -19,18 +20,16 @@ const Header = ({ title, onAdd, showAdd }) => {
   );
 };
 
+
 Header.defaultProps = {
   title: "Task Manager"
 };
+
 
 Header.propTypes = {
   title: PropTypes.string.isRequired
 };
 
-// CSS in JS
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black',
-// }
+
 
 export default Header;
