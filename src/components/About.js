bb7547401bc2ebee7b96e01a47a6ed72bd39f034
Chirag Mehta <61863283+chiragmehta900@users.noreply.github.com>
2021-12-16 21:22:25 +0530

    code upload

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <hr />
+      <br />
+      <br />
+      <h4>Mehta Chandni</h4>
+      <p>
+        Full Stack <br /> 144-A
+      </p>
+      <br />
+      <br />
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Assignment 8</p>
+      <Link to="/about">About</Link>
+    </footer>
+  );
+};
+
+export default Footer;
